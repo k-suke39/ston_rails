47a9f4d80f77a708b36cd858eda54649d9d40f75
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-04 16:47:21 +0900

    add rpsec

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -6,10 +6,24 @@ RSpec.describe "Posts", type: :system do
   end
 
   describe 'index page' do
-    context 'when displaying idex page' do
+    context 'when displaying index page' do
       let!(:post) { create(:post) }
       it 'displayed the content' do
         visit posts_index_path
+        expect(page).to have_content post.content
+      end
+    end
+  end
+
+  describe 'show page' do
+    context 'when displaying show page' do
+      let!(:post) { create(:post) }
+      it 'displayed the id' do
+        visit "/posts/#{post.id}"
+        expect(page).to have_content post.id
+      end
+      it 'displayed the content' do
+        visit "/posts/#{post.id}"
         expect(page).to have_content post.content
       end
     end
